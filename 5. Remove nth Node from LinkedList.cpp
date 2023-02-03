85f9cf5fd88534fabd92c31044741831a4c067ee
Manshi Malik <112588239+manshi23@users.noreply.github.com>
2023-02-03 11:20:40 +0530

    Create 5. Remove nth Node from LinkedList.cpp

---
+++ 5. Remove nth Node from LinkedList.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int length(ListNode*head){
+        ListNode*temp=head;
+        int len=0;
+        while(temp!=NULL){
+            len++;
+            temp=temp->next;
+        }
+        return len;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(n==0||head==NULL){
+            return head;
+        }
+        int len=length(head);
+        ListNode*temp=head;
+        if(len-n==0){
+            return head->next;
+        }
+        for(int i=0;i<len-n-1;i++){
+            temp=temp->next;
+        }
+        ListNode*a=temp->next;
+        if(temp&&a){
+            temp->next=a->next;
+        }
+        return head;
+    }
+};
